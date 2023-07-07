4e827889811d9e6470f2ae1ebb3a298c31f88448
Per Næss <perness3009@gmail.com>
2023-07-07 18:53:25 +0200

    feat: add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contains it
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    #[arg(short, long)]
+    pattern: String,
+
+    /// The path to the file to look in
+    #[arg(short = 'f', long)]
+    filepath: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
