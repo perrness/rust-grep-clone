b01c6726f81f9156c4df322c92b24f3f54a85424
Per Næss <perness3009@gmail.com>
2023-07-08 14:51:40 +0200

    feat: add another test

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,7 @@
 use std::process::Command;
 
 use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
+use assert_fs::prelude::FileWriteStr;
 use predicates::prelude::predicate;
 
 #[test]
@@ -14,6 +15,23 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("rust-grep-clone")?;
+    cmd.arg("--pattern")
+        .arg("test")
+        .arg("--filepath")
+        .arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicates::str::contains("test\nAnother test"));
 
     Ok(())
 }
